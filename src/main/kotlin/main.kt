a85d9e2ddfbf847ec3d1190de7f5822e40ab32c3
Erin Becker <erin.becker23@d155.org>
2022-08-17 08:09:16 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var int: Int 8
+    var long: Long = 12345
+    var float: Float = 23.1
+    var double: Double = 5678.4231
+    var boolean: Boolean = false
+    var char: Char = 'e'
+    var byte: Byte = 1
+
+
+
+}
