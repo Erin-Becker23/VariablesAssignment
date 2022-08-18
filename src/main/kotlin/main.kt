512be5b18de944c712f6a196b8b5ea24a5e672f2
Erin Becker <erin.becker23@d155.org>
2022-08-18 07:57:57 -0500

    updated

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,21 @@
 fun main(){
-    var int: Int 8
-    var long: Long = 12345
-    var float: Float = 23.1
+    var int: Int = 10
+    var long: Long = 12345L
+    var float: Float = 23.18766F
     var double: Double = 5678.4231
     var boolean: Boolean = false
     var char: Char = 'e'
     var byte: Byte = 1
 
+var multiline = """
+    Int: $int
+    Long: $long
+    Float: $float
+    Double $double
+    Boolean: $boolean
+    Char: $char
+    Byte: $byte 
+""".trimIndent()
 
-
+    print(multiline)
 }
